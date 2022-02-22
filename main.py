85082f2459cfa07823d061a61d2e61265576d9e7
Dat Nguyen <cddd4f@yahoo.com>
2022-02-22 21:28:50 +0700

    all

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 
-from flask import Flask,render_template
+from flask import Flask,render_template,flash
+from flask_wtf import FlaskForm 
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 
 app= Flask(__name__)
-
+app.config["SECRET_KEY"] = "my supper secret key that no one is supposed to know"
+#Form
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name",validators=[DataRequired()])
+    submit=SubmitField("Submit")
 @app.route('/')
 
 def index():
@@ -16,7 +23,7 @@ def index():
     first_name="Nguyen"
     # stuff= "This Is <strong> Bold </strong>Text"
     stuff= "This Is bold Text"
-    
+    flash("Welcom To Our Website!")
     favorite_pizza=["Pepperoni","Cheese","Mushrooms",41]
     return render_template("index.html",first_name=first_name,
                             stuff=stuff,
@@ -34,6 +41,20 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("500.html"),500
 
+@app.route('/name',methods=["GET", "POST"])
+def  name():
+    name=None
+    form =NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=""
+        flash("Form submitted Successfully")
+    return render_template("name.html",
+                            name=name,
+                            form=form)
+
+
+
 if __name__== '__main__':
     app.run(debug=True)
 # git config --global user.name "Dat Nguyen"
@@ -46,8 +67,11 @@ if __name__== '__main__':
 # git remote add origin https://github.com/quangdat385/quangdat385.git
 # 
 # 
-# git push -u origin main
+# git push -u origin main'
+# git add .
 # git commit -am "tweaked 500.html"
+
+# git pull
 #  git push
 
 # run flask
